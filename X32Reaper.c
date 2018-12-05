c7c227fb07b04ed8fb38131cc33f052f7c654711
Patrick-Gilles Maillot <patrick.maillot@gmail.com>
2018-12-05 08:07:59 +0100

    Update X32Reaper.c

--- X32Reaper.c
+++ X32Reaper.c
@@ -1,3 +1,4 @@
+
 /*
  * X32Reaper.c
  *
@@ -111,10 +112,10 @@ extern int Xfprint(char *bd, int index, char* text, char format, void *bs);
 	do {																\
 		if (Xverbose) Xlogf("->X", Xb_s, Xb_ls);						\
 		if (sendto(Xfd, Xb_s, Xb_ls, 0, XX32IP_pt, XX32IP_len) < 0) {	\
-			fprintf(log_file, errorX32);								\
+			fprintf(log_file, "Error sending data to X32\n");			\
 			exit(EXIT_FAILURE);											\
 		} 																\
-		if (delay > 0) Sleep(delay);									\
+		if (delay > 0) MySleep(delay);									\
 	} while (0);
 //
 //
@@ -122,7 +123,7 @@ extern int Xfprint(char *bd, int index, char* text, char format, void *bs);
 	do {																\
 		if (Xverbose) Xlogf("->R", Rb_s, Rb_ls);						\
 		if (sendto(Rfd, Rb_s, Rb_ls, 0, RHstIP_pt, RHstIP_len) < 0) {	\
-			fprintf(log_file, errorRea);								\
+			fprintf(log_file, "Error sending data to REAPER\n");		\
 			exit(EXIT_FAILURE);											\
 		} 																\
 	} while (0);
@@ -335,7 +336,7 @@ int main(int argc, char **argv) {
 				if (Xfd > Rfd) Mfd = Xfd + 1;
 				if (select(Mfd, &fds, NULL, NULL, &timeout) > 0) {
 					if (FD_ISSET(Rfd, &fds) > 0) {
-						if ((Rb_lr = recvfrom(Rfd, Rb_r, RBrmax, 0, RFrmIP_pt, &RFrmIP_len)) > 0) {
+						if ((Rb_lr = recvfrom(Rfd, Rb_r, RBrmax, 0, RFrmIP_pt, (unsigned int*)&RFrmIP_len)) > 0) {
 // Parse Reaper Messages and send corresponding data to X32
 // These can be simple or #bundle messages!
 // Can result in several/many messages to X32
@@ -345,7 +346,7 @@ int main(int argc, char **argv) {
 						}
 					}
 					if (FD_ISSET(Xfd, &fds) > 0) {
-						if ((Xb_lr = recvfrom(Xfd, Xb_r, XBrmax, 0, XX32IP_pt, &XX32IP_len)) > 0) {
+						if ((Xb_lr = recvfrom(Xfd, Xb_r, XBrmax, 0, XX32IP_pt, (unsigned int*)&XX32IP_len)) > 0) {
 // X32 transmitted something
 // Parse and send (if applicable) to REAPER
 //							printf("X32 sent data\n"); fflush(stdout);
